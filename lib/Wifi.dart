f7e218093404e16277057cb8a10be40d5fbe5d75
terrypls <selvastian@gmail.com>
2019-01-14 18:02:45 -0300

    wifi casi listo

--- lib/Wifi.dart
+++ lib/Wifi.dart
@@ -9,23 +9,53 @@ class wifi extends StatefulWidget{
 class wifiApp extends State<wifi>{
   String wifiName;
   String ip;
+  String intensidad;
   Future<Null> _getIP()async{
-    String ip= await Wifi.ip;
+    String _ip= await Wifi.ip;
     setState(() {
-      ip=ip;
+      ip=_ip;
     });
   }
   Future<Null> _getWifiName()async{
-    String wifiName = await Wifi.ssid;
+    String _wifiName = await Wifi.ssid;
     setState(() {
-      wifiName =wifiName;
+      wifiName =_wifiName;
     });
+  }
+  Future<Null> _getWifiLevel()async{
+  String _wifiLevel = await Wifi.level.toString();
+  setState(() {
+    intensidad = _wifiLevel;
+  });
+  }
+
+  getData(){
+    print(1);
+    child: new Column(
+      children: <Widget>[
+        new Text(wifiName),
+        new Text(ip),
+        new Text(intensidad),
+
+      ],
+    );
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Wifi'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('get wifi info'),
+              onPressed: getData,
+            ),
+
+          ],
+        ),
       ),
     );
   }
